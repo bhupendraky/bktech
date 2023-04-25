95ad2f813c9fdca23d3831ae3d0ae0d6c1a12fc4
Bhupendra Kumar <bhupendraky@gmail.com>
2023-04-25 16:33:55 +0530

    JDBC Auth Security Config updated with proper url pattern and sql

--- app-user-service/src/main/java/com/bktech/user/security/JdbcAuthSecurityConfig.java
+++ app-user-service/src/main/java/com/bktech/user/security/JdbcAuthSecurityConfig.java
@@ -4,9 +4,14 @@ import javax.sql.DataSource;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import com.bktech.user.constants.RoleType;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,23 +21,38 @@ import lombok.RequiredArgsConstructor;
 public class JdbcAuthSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	private final DataSource dataSource;
+	private final PasswordEncoder passwordEncoder;
+	private final AuthenticationEntryPoint authEntryPoint;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.jdbcAuthentication()
 		.dataSource(dataSource)
-		.usersByUsernameQuery("select user_name as username, password "
+		.usersByUsernameQuery("select user_name as username, user_pwd as password, 1 as enabled "
 				+ " from users where user_name=?")
-		.authoritiesByUsernameQuery("select user_name as username, authority "
-				+ " from authorities where user_name=?");
+		.authoritiesByUsernameQuery("select u.user_name as username, r.name as authority "
+				+ " from users u, roles r, user_roles ur where r.id=ur.role_id "
+				+ " and u.id=ur.user_id and u.user_name=?")
+		.passwordEncoder(passwordEncoder)
+		;
 	}
 
-	protected void configure2(AuthenticationManagerBuilder auth) throws Exception {
-		auth.jdbcAuthentication()
-		.dataSource(dataSource)
-		.withDefaultSchema()
-		.withUser(User.withUsername("BK").password("BK").roles("ADMIN"))
-		.withUser(User.withUsername("RK").password("RK").roles("USER"));
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.csrf().disable()
+		.authorizeRequests()
+		.antMatchers("/api/user/**").hasAuthority(RoleType.USER.name())
+		.antMatchers("/api/admin/**").hasAuthority(RoleType.ADMIN.name())
+		.anyRequest().authenticated()
+		.and()
+		.exceptionHandling()
+		.authenticationEntryPoint(authEntryPoint)
+		.and()
+		.sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and()
+		.httpBasic();
 	}
 
 }
